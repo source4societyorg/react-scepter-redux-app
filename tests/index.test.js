74e6c158226ca7d4477be0d9918f0501eb8189a5
Daniel Fredriksen <dfredriksen@cyint.technology>
2018-03-24 09:07:28 -0700

    Fixed react-router-redux version

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import createHistory from 'history/createMemoryHistory';
-import { ScepterReduxApp } from '../src/index';
+import ScepterReduxApp from '../src/index';
 test('ScepterReduxApp renders without problems', () => {
   const history = createHistory();
   const MyApp = () => <div />;
-  shallow(<ScepterReduxApp MyApp={MyApp} history={history} />);
+  const renderComponent = mount(<ScepterReduxApp MyApp={MyApp} history={history} />);
+  renderComponent.render();
 });
